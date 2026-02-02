94a0b41a4c4b51dd2b659945bcb162fe8fc82f62
Joshua Toyota <jearsh+github@gmail.com>
2026-02-02 18:59:31 -0500

    publishing v0.1.39

--- plugins/tasks.rb
+++ plugins/tasks.rb
@@ -763,14 +763,6 @@ module Tasks
     bin_name = [parent_hiiro.bin, parent_hiiro.subcmd || ''].join('-')
 
     Hiiro.init(bin_name:, args: parent_hiiro.args) do |h|
-      puts "parent_hiiro: all_args: #{parent_hiiro.all_args}"
-      puts "parent_hiiro: parent_cmd: #{parent_hiiro.subcmd}"
-      puts "parent_hiiro: args: #{parent_hiiro.args}"
-
-      puts "subhiiro: all_args: #{h.all_args}"
-      puts "subhiiro: subcmd: #{h.subcmd}"
-      puts "subhiiro: args: #{h.args}"
-
       h.add_subcmd(:list) { mgr.list }
       h.add_subcmd(:ls) { mgr.list }
 
