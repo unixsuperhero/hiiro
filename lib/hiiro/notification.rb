5ca2459b6e08da965f1cbb795127365327ab0466
Joshua Toyota <jearsh+github@gmail.com>
2026-02-12 22:46:44 -0500

    publishing v0.1.77

--- lib/hiiro/notification.rb
+++ lib/hiiro/notification.rb
@@ -1,6 +1,6 @@
 class Hiiro
   class Notification
-    def self.send(hiiro)
+    def self.show(hiiro)
       new(hiiro).send
     end
 
@@ -11,18 +11,43 @@ class Hiiro
       @original_args = hiiro.args.dup
     end
 
+    def options
+      @options ||= Options.parse(args) do
+        option(:sound, short: :s, default: 'basso', desc: 'sound name')
+        option(:title, short: :t, desc: 'title')
+        option(:message, short: :m, desc: 'message')
+        option(:link, short: :l, desc: 'link to open')
+        option(:command, short: :c, desc: 'command to run')
+      end
+    end
+
     def args
       @args ||= hiiro.args
     end
 
     def sounds
-      system = Dir.glob('/System/Library/Sounds/*')
       custom = Dir.glob(File.join(Dir.home, '.config/hiiro/sounds/*'))
 
-      all_sounds = system + custom
-      all_sounds.each_with_object({}) do |fn, h|
+      @sounds ||= custom.each_with_object({}) do |fn, h|
         basename = File.basename(fn, File.extname(fn))
-        puts basename: basename
+        h[basename.downcase] = fn
+      end
+    end
+
+    def show
+      cmd = ['terminal-notifier']
+      cmd << ['-message', options.message] if options.message
+      cmd << ['-title', options.title.tr('()[]', '')] if options.title
+      cmd << ['-open', options.link] if options.link
+      cmd << ['-execute', options.command] if options.command
+      system(*cmd)
+
+      play_sound
+    end
+
+    def play_sound
+      if options.sound && sounds[options.sound]
+        system('afplay', sounds[options.sound.downcase])
       end
     end
   end
