9d65efde40f5c32c10149e05605d5ba8e738fccd
Joshua Toyota <joshua.toyota@instacart.com>
2026-02-27 05:01:14 -0500

    misc changes

---
+++ sync.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+safe_nvim -d bin/h-app /Users/josh/bin/h-app
+safe_nvim -d bin/h-branch /Users/josh/bin/h-branch
+safe_nvim -d bin/h-link /Users/josh/bin/h-link
+safe_nvim -d bin/h-pr /Users/josh/bin/h-pr
+safe_nvim -d bin/h-session /Users/josh/bin/h-session
+safe_nvim -d bin/h-window /Users/josh/bin/h-window
+safe_nvim -d plugins/tasks.rb /Users/josh/.config/hiiro/plugins/tasks.rb
