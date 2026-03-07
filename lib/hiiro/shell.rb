8f50640ea78e8f07e78889aa825ce97830ec8795
Joshua Toyota <joshua.toyota@instacart.com>
2026-03-07 00:20:09 -0500

    pass envvar w/ ruby version when updating hiiro

--- lib/hiiro/shell.rb
+++ lib/hiiro/shell.rb
@@ -2,6 +2,13 @@ require 'open3'
 
 class Hiiro
   class Shell
+    def self.capture_output(*command, **env)
+      env = env.transform_keys(&:to_s).transform_values(&:to_s)
+
+      stdout, status = Open3.capture2(env, *command)
+      stdout
+    end
+
     def self.pipe_lines(lines, *command)
       content = lines.is_a?(Array) ? lines.join("\n") : lines.to_s
 
