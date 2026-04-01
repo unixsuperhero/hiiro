63af92a26c00a0b2b45d848db5f645b29a8b8fd1
Joshua Toyota <joshua.toyota@instacart.com>
2026-04-01 02:17:49 -0400

    feat: add optional directory parameter to Background.run for chdir support

--- lib/hiiro/background.rb
+++ lib/hiiro/background.rb
@@ -5,12 +5,17 @@ class Hiiro
     # Run cmd asynchronously. Inside tmux, spins up a hidden window in the
     # h-bg session so you can attach and inspect if needed. Outside tmux,
     # falls back to a detached spawn.
-    def self.run(*cmd)
+    #
+    # dir: optional directory to cd into before running the command.
+    def self.run(*cmd, dir: nil)
       if inside_tmux?
         ensure_session
-        system('tmux', 'new-window', '-d', '-t', "#{SESSION}:", '-n', cmd.first.to_s, cmd.shelljoin)
+        tmux_args = ['tmux', 'new-window', '-d', '-t', "#{SESSION}:", '-n', cmd.first.to_s]
+        tmux_args += ['-c', dir] if dir
+        system(*tmux_args, cmd.shelljoin)
       else
-        Process.detach(spawn(*cmd))
+        spawn_opts = dir ? { chdir: dir } : {}
+        Process.detach(spawn(*cmd, **spawn_opts))
       end
     rescue
       nil
